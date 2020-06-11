d825406b0681d132bfda6e6820607ec47ab72c74
frostyworks <fiddleginger@gmail.com>
2020-06-11 15:28:10 +0200

    add a function to check if a string contain numbers

--- modules/components.py
+++ modules/components.py
@@ -1,6 +1,10 @@
 """This module acts as our constants, but we
    will call it components.py"""
 
-
 user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 darksky_line = "https://api.darksky.net/forecast/abe6a84811a8ab8f1f39cd9b8b8f40e1/{},{}"
+
+
+def check_digits(s):
+    """Check if a string contains digit and return true"""
+    return any(i.isdigit() for i in s)
