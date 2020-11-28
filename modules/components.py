6dc39d94f09a3acda9f4cc3e3a31ecc56fa75514
frostyworks <fiddleginger@gmail.com>
2020-11-28 22:38:48 +0100

    ბოტი ახლა უგზავნის მომხმარებლებს welcome მესიჯს ქართულად

--- modules/components.py
+++ modules/components.py
@@ -3,7 +3,7 @@
 
 user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 darksky_line = "https://api.darksky.net/forecast/abe6a84811a8ab8f1f39cd9b8b8f40e1/{},{}"
-welcome_line = "Liberty City Roleplay is a medium / heavy English text-only roleplay server which focuses itself on its high standard of roleplay and custom advanced scripts made possible with the help of the Underground Multiplayer mod known as UG:MP. The community consists group of experienced people from all over the world that came from different places and platforms. We are aiming to provide a realistic and enjoyable roleplay environment for all to enjoy and everyone is welcome to come sit down with us and experience what our community has to offer."
+welcome_line = "Hype Roleplay არის ერთ ერთი ახალი საუკეთესო ქართული სერვერი რომელიც მიმდინარეობს ვირტუალურ ქართულ სამყაროში, ქალაქ თბილიშში. ჩვენი სერვერი ხმარობს GTA V კლიენტს რომელსაც ქვია RAGE MULTIPLAYER."
 
 
 def check_digits(s):
